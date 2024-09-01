04492693286fb3aad56111efce9388e27e838c54
Brued <Bruedbes@gmail.com>
2024-08-31 23:20:14 -0300

    ex9

---
+++ bootcamp_python/a4/ex9.py
@@ -0,0 +1,9 @@
+#  Extração de Chaves e Valores
+# Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
+
+dic = {"Poder" : "Power", "Reino": "Kingdom", "Glória": "Glory" } 
+chaves = [dic.keys()]
+valores = [dic.values()]
+
+print(f"Chaves: {chaves}")
+print(f"Valores: {valores}")
