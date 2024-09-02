31bee63ed5bb19db5c2e3661a8683f987a7b3287
Brued <Bruedbes@gmail.com>
2024-09-02 20:49:31 -0300

    ex15

--- bootcamp_python/a4/ex15.py
+++ bootcamp_python/a4/ex15.py
@@ -1,1 +1,18 @@
 # Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
+
+
+
+
+def keys_order(dict: dict):
+    order = sorted(dict.keys())
+    return order
+
+
+
+dicionario = {
+    'alpha': 1200,
+    'delta': 1003,
+    'gamma': 222,
+    'betha': 104
+}
+print(keys_order(dicionario))
